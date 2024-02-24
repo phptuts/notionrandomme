6c7baa9caa882d9f9bedb3ac5e3dbe0781071219
Noah-Glaser <noah.glaser@docusign.com>
2024-02-23 23:32:50 -0800

    Create Page

---
+++ README.md
@@ -0,0 +1,13 @@
+# SETUP
+
+## Integrations
+
+1\. Go here and click on + integrations https://www.notion.so/my-integrations
+
+2\. Grab your secret once you have created your integration.
+
+3\. Add it to a file called .env under NOTION_API_SECRET={YOUR_SECRET}
+
+4\. Connect peeps to your page.
+
+![peeps](docs/peeps.png)
--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const https = require('https');
+const { Client }  = require("@notionhq/client")
+const { config } = require("dotenv")
+
+config();
+const apiKey = process.env.NOTION_API_KEY;
+const rootPageId = process.env.ROOT_PAGE_ID;
+const notion = new Client({ auth: apiKey })
+
 const app = express();
 
 async function getRandomUser() {
@@ -10,10 +18,31 @@ async function getRandomUser() {
     return json;
 }
 
+async function createPage(title) {
+    try {
+        const response = await notion.pages.create({
+            parent: { page_id: rootPageId },
+            properties: {
+                title: [
+                    {
+                        text: {
+                            content: title
+                        }
+                    }
+                ]
+            }
+        });
+        return response.id;
+    } catch (error) {
+        console.error("Error creating page:", error.body);
+        throw error;
+    }
+}
 
 app.get('/data', async (request, response) => {
-    const person = await getRandomUser();
-    response.json({ 'success': true, person })
+    const page = await createPage('Example Page');
+    const user = await getRandomUser();
+    response.json({ 'success': true, page, user })
 });
 
 
