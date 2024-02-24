a95183b6521be4db48aa6aa0975d127ab19c9bf9
Noah-Glaser <noah.glaser@docusign.com>
2024-02-23 22:06:06 -0800

    Got Person to load in the api

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const https = require('https');
 const app = express();
 
-app.get('/data', (request, response) => {
+async function getRandomUser() {
+    const url = 'https://randomuser.me/api';
+    const response = await fetch(url);
+    const json = await response.json();
 
-    response.json({ 'success': true})
-})
+    return json;
+}
+
+
+app.get('/data', async (request, response) => {
+    const person = await getRandomUser();
+    response.json({ 'success': true, person })
+});
+
+
 
 app.listen(3000, () => {
     console.log('listening');
