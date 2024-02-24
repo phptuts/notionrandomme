5497bcd737f6fff63fb5b6315900b8c32e6e41b4
Noah-Glaser <noah.glaser@docusign.com>
2024-02-23 23:53:57 -0800

    Got everything working

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const notion = new Client({ auth: apiKey })
 const app = express();
 
 async function getRandomUser() {
-    const url = 'https://randomuser.me/api';
+    const url = 'https://randomuser.me/api?nat=us';
     const response = await fetch(url);
     const json = await response.json();
 
@@ -39,10 +39,76 @@ async function createPage(title) {
     }
 }
 
+function createCell(text) {
+    return [
+        {
+          "type": "text",
+          "text": {
+            "content": text,
+            "link": null
+          },
+          "annotations": {
+            "bold": false,
+            "italic": false,
+            "strikethrough": false,
+            "underline": false,
+            "code": false,
+            "color": "default"
+          },
+          "plain_text": text,
+          "href": null
+        }
+      ];
+}
+
+function createRow(cellText1, cellText2) {
+    return {
+        type: "table_row",
+        table_row: {
+            cells: [
+               createCell(cellText1),
+               createCell(cellText2)
+            ],
+        }
+    }
+}
+
+async function createTable(pageId, rows) 
+{
+    const children = [
+        createRow('Key', 'Value'),
+        ...rows.map(keyValue => {
+            return createRow(keyValue.key, keyValue.value);
+    })];
+    await notion.blocks.children.append({
+        block_id: pageId,
+        children: [
+            {
+                object: 'block',
+                type: 'table',
+                table: {
+                    table_width: 2,
+                    has_row_header: true,
+                    children
+                }
+            }
+        ]
+    });
+}
+
 app.get('/data', async (request, response) => {
-    const page = await createPage('Example Page');
-    const user = await getRandomUser();
-    response.json({ 'success': true, page, user })
+    const data = await getRandomUser();
+    const user = data.results[0];
+    const pageName = `${user.name.first}'s Page`;
+    const pageId = await createPage(pageName);
+    const rows = [
+        {key: 'Name', value: `${user.name.first} ${user.name.last}` },
+        {key: 'Gender', value: user.gender},
+        {key: 'Country', value: user.location.country},
+        {key: 'Email', value: user.email},
+    ]
+    createTable(pageId, rows);
+    response.json({ 'success': true })
 });
 
 
